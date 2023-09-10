c12a869182067448fc38b27c9cf4c86a57646eaa
marcelolimad <99409470+marcelolimad@users.noreply.github.com>
2023-09-10 16:20:53 -0300

    Initial commit

---
+++ src/app/components/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+
+ selector:'my-button',
+ templateUrl:'./button.component.html',
+ styleUrls: ['./button.component.css'],
+  
+
+})
+export class Button {
+   
+ @Input() label: string = '';
+  buttonText: string = 'ACESSAR';
+  btnumero: number = 1;
+  btvetor: string[] = ['TESTE1', 'TESTE2'];
+ btObject = {
+ label: 'add ao carrinho',
+ label2: 'label2',
+
+ }
+
+}
---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+ selector: 'menu-component',
+ template: '<h2>Marcelo<h2>',
+ styles: ['h2{color: blue}'],
+  
+
+})
+export class MenuComponent {
+
+
+}
